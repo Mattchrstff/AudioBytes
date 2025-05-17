1ed10fd9b6d6efa8efac24df8f52adb1a6e38152
Nathan Headrick <156489706+nheadrick@users.noreply.github.com>
2025-05-17 14:58:23 -0700

    Android Fixed
    See top. Basic ide working

--- Builds/AdroidOS/app/build/intermediates/cxx/Debug/5532b314/logs/x86_64/build_command_audiobytes.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Twitchy2654\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Twitchy2654\\Desktop\\AudioBytes\\Builds\\AdroidOS\\app\\.cxx\\Debug\\5532b314\\x86_64" ^
-  audiobytes
